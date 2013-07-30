90cafd09e775682d0be633fbfbce2655a92aebe5
Dabo Ross <daboross@daboross.net>
2013-07-29 18:18:48 -0700

    Initial Commit

---
+++ src/main/java/net/daboross/bukkitdev/skywars/SkyWarsPlugin.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2013 Dabo Ross <http://www.daboross.net/>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.daboross.bukkitdev.skywars;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author daboross
+ */
+public class SkyWarsPlugin extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("")) {
+        } else {
+            sender.sendMessage("SkyWars doesn't know about the command /" + cmd.getName());
+        }
+        return true;
+    }
+}
