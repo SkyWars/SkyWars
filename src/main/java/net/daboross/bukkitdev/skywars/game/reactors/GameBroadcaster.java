f2f7be000235b7fc4a42ff4cb9bd998760dda387
Dabo Ross <dabodialga@gmail.com>
2013-08-20 16:32:46 -0700

    Refractor

--- src/main/java/net/daboross/bukkitdev/skywars/game/reactors/GameBroadcaster.java
+++ src/main/java/net/daboross/bukkitdev/skywars/game/reactors/GameBroadcaster.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package net.daboross.bukkitdev.skywars.listeners;
+package net.daboross.bukkitdev.skywars.game.reactors;
 
 import java.util.List;
 import net.daboross.bukkitdev.commandexecutorbase.ColorList;
