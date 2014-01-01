2ecedd1daed8400ea110bd368f52329874be217b
Dabo Ross <daboross@daboross.net>
2014-01-01 02:00:27 -0800

    Update copyrights

--- src/main/java/net/daboross/bukkitdev/skywars/commands/setupsubcommands/StartNewArena.java
+++ src/main/java/net/daboross/bukkitdev/skywars/commands/setupsubcommands/StartNewArena.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 Dabo Ross <http://www.daboross.net/>
+ * Copyright (C) 2013-2014 Dabo Ross <http://www.daboross.net/>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
