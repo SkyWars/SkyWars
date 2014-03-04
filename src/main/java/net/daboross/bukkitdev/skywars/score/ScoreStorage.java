4da2af198658bc051decf6587b7e5db45b804b3e
Dabo Ross <daboross@daboross.net>
2014-03-04 01:11:01 -0800

    And I should be compiling locally before pushing .-. - at least TeamCity caught that

--- src/main/java/net/daboross/bukkitdev/skywars/score/ScoreStorage.java
+++ src/main/java/net/daboross/bukkitdev/skywars/score/ScoreStorage.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package net.daboross.bukkitdev.skywars.points;
+package net.daboross.bukkitdev.skywars.score;
 
 import java.io.IOException;
 import java.lang.reflect.Constructor;
