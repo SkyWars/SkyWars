63dbfb472ee8356c772105f2db7039abf335a86a
Dabo Ross <daboross@daboross.net>
2014-05-19 12:43:31 -0700

    Fix team scoreboards, also add some debugging in scoreboards.

--- src/main/java/net/daboross/bukkitdev/skywars/scoreboards/TeamScoreboardListener.java
+++ src/main/java/net/daboross/bukkitdev/skywars/scoreboards/TeamScoreboardListener.java
@@ -18,13 +18,14 @@ package net.daboross.bukkitdev.skywars.scoreboards;
 
 import java.util.HashMap;
 import java.util.Map;
+import net.daboross.bukkitdev.skywars.api.SkyStatic;
 import net.daboross.bukkitdev.skywars.api.game.SkyGame;
 import net.daboross.bukkitdev.skywars.events.events.GameEndInfo;
 import net.daboross.bukkitdev.skywars.events.events.GameStartInfo;
 import net.daboross.bukkitdev.skywars.events.events.PlayerLeaveGameInfo;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Server;
+import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
 
@@ -36,18 +37,21 @@ public class TeamScoreboardListener {
     public void onGameStart(GameStartInfo info) {
         SkyGame game = info.getGame();
         if (game.areTeamsEnabled()) {
-            Server server = Bukkit.getServer();
-            Scoreboard board = server.getScoreboardManager().getNewScoreboard();
+            SkyStatic.debug("Setting up teams for game %s", game.getId());
+            Scoreboard board = Bukkit.getScoreboardManager().getNewScoreboard();
             scoreboards.put(game.getId(), board);
             for (int teamNum = 0, max = game.getNumTeams(); teamNum < max; teamNum++) {
                 String teamName = "Team " + teamNum;
                 Team team = board.registerNewTeam(teamName);
                 team.setAllowFriendlyFire(false);
                 team.setCanSeeFriendlyInvisibles(true);
-                team.setPrefix(ChatColor.GRAY + "[" + ChatColor.DARK_RED + teamNum + ChatColor.GRAY + "]");
+                team.setPrefix(ChatColor.GRAY + "[" + ChatColor.DARK_RED + teamNum + ChatColor.GRAY + "]" + ChatColor.WHITE + " ");
                 for (String name : game.getAllPlayersInTeam(teamNum)) {
-                    team.addPlayer(server.getPlayerExact(name));
+                    SkyStatic.debug("Adding %s to team %s", name, teamName);
+                    Player player = Bukkit.getPlayerExact(name);
+                    team.addPlayer(player);
                     teams.put(name.toLowerCase(), team);
+                    player.setScoreboard(board);
                 }
             }
         }
