9c1b8d092fd70dc0594f551dbfe234067aae5050
Dabo Ross <daboross@daboross.net>
2016-02-11 13:07:14 -0800

    Add team starting messages to tell each player who their teammates are

--- src/main/java/net/daboross/bukkitdev/skywars/events/listeners/GameBroadcaster.java
+++ src/main/java/net/daboross/bukkitdev/skywars/events/listeners/GameBroadcaster.java
@@ -16,7 +16,10 @@
  */
 package net.daboross.bukkitdev.skywars.events.listeners;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
+import net.daboross.bukkitdev.skywars.api.game.SkyGame;
 import net.daboross.bukkitdev.skywars.api.translations.SkyTrans;
 import net.daboross.bukkitdev.skywars.api.translations.TransKey;
 import net.daboross.bukkitdev.skywars.events.events.GameEndInfo;
@@ -38,6 +41,37 @@ public class GameBroadcaster {
             playerNames.append(players.get(i).getName());
         }
         Bukkit.broadcastMessage(SkyTrans.get(TransKey.GAME_STARTING_GAMESTARTING, playerNames));
+        // TODO: should team message broadcasts really be in here?
+        SkyGame game = info.getGame();
+        if (game.areTeamsEnabled()) {
+            // probably a much more efficient way to do this than all these loops, but
+            // this seems the least convoluted way to broadcast team messages.
+            for (int teamId = 0; teamId < game.getNumTeams(); teamId++) {
+                SkyGame.SkyGameTeam team = game.getTeam(teamId);
+                List<Player> teamPlayers = new ArrayList<>(team.getPlayers().size());
+                for (UUID uuid : team.getPlayers()) {
+                    teamPlayers.add(Bukkit.getPlayer(uuid));
+                }
+                for (Player p : teamPlayers) {
+                    List<String> teamNames = new ArrayList<>(teamPlayers.size() - 1);
+                    for (Player teamPlayer : teamPlayers) {
+                        if (teamPlayer != p) {
+                            teamNames.add(teamPlayer.getName());
+                        }
+                    }
+                    StringBuilder nameBuilder = new StringBuilder(teamNames.get(0));
+                    for (int i = 1; i < teamNames.size(); i++) {
+                        if (i < teamNames.size() - 1) {
+                            nameBuilder.append(SkyTrans.get(TransKey.GAME_STARTING_TEAM_COMMA));
+                        } else {
+                            nameBuilder.append(SkyTrans.get(TransKey.GAME_STARTING_TEAM_FINAL_COMMA));
+                        }
+                        nameBuilder.append(teamNames.get(i));
+                    }
+                    p.sendMessage(SkyTrans.get(TransKey.GAME_STARTING_TEAM_MESSAGE, team.getName(), nameBuilder));
+                }
+            }
+        }
     }
 
     public void broadcastEnd(GameEndInfo info) {
